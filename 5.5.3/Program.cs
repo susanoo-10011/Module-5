d5b931c279e0d54b9629b08534ef705defa396d2
susanoo-10011 <cycahosir@gmail.com>
2023-04-15 19:18:57 +0700

    рекурсивный метод

--- 5.5.3/Program.cs
+++ 5.5.3/Program.cs
@@ -10,7 +10,33 @@ namespace Module_5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Напишите что-то");
+            var str = Console.ReadLine();
 
+            Console.WriteLine("Укажите глубину эха");
+            var deep = int.Parse(Console.ReadLine());
+
+            Echo(str, deep);
+
+            Console.ReadKey();
         }
+
+        static void Echo(string saidworld, int deep)
+        {
+            var modif = saidworld;
+
+            if (modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2);
+            }
+
+            Console.WriteLine("..." + modif);
+
+            if (deep > 1)
+            {
+                Echo(modif, deep - 1);
+            }
+        }
+
     }
 }
